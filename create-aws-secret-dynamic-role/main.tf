0db185bae776e1879b87a2186b54fe0096f924a8
vagrant <vagrant@hellocloud-native-box>
2024-09-24 08:23:56 +0000

    Modified create-aws-secret-dynamic-role/main.tf

--- create-aws-secret-dynamic-role/main.tf
+++ create-aws-secret-dynamic-role/main.tf
@@ -2,7 +2,7 @@ resource "vault_aws_secret_backend" "aws" {
   access_key = aws_iam_access_key.vault_root.id
   secret_key = aws_iam_access_key.vault_root.secret
   region = "ap-southeast-1"
-  path = aws-master
+  path = "aws-master"
   default_lease_ttl_seconds = 900
   max_lease_ttl_seconds = 2000
 }
