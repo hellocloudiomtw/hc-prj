e449ea7d121f018199f7f34802b582794170e8fe
vagrant <vagrant@hellocloud-native-box>
2024-09-26 14:30:00 +0000

    update private & public subnets

--- create-vpc-subnet-rt-igw-ngw/main.tf
+++ create-vpc-subnet-rt-igw-ngw/main.tf
@@ -21,3 +21,111 @@ resource "aws_subnet" "public_subnets" {
     Name = "public-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
   }
 }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.public_igw.id
+}
+
+resource "aws_internet_gateway" "public_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-igw"
+  }
+}
+
+###################################################
+### Private subnet
+##################################################
+
+resource "aws_subnet" "private_subnets" {
+  count = length(var.private_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+
+  tags = {
+    Name = "private-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_internet_gateway
+}
+
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+  depends_on = [ aws_internet_gateway.public_igw ]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public_subnets[0].public_igw
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  depends_on = [ aws_internet_gateway.public_igw ]
+  
+}
+
+###################################################
+### db subnet
+##################################################
+
+resource "aws_subnet" "db_subnets" {
+  count = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+
+  tags = {
+    Name = "db-subnet-0${count.index+1}-${data.aws_availability_zones.azs.names[count.index]}"
+  }
+}
+
+resource "aws_route_table" "db_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "db-route-table"
+  }
+}
+
+resource "aws_route_table_association" "db" {
+  count = length(var.db_subnets)
+  subnet_id      = aws_subnet.db_subnets[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
--- create-vpc-subnet-rt-igw-ngw/variable.tf
+++ create-vpc-subnet-rt-igw-ngw/variable.tf
@@ -3,4 +3,14 @@ variable public_subnets {
   description = "public_subnets"
 }
 
+variable private_subnets {
+  default = ["10.10.4.0/24","10.10.5.0/24","10.10.6.0/24"]
+  description = "private_subnets"
+}
+
+variable db_subnets {
+  default = ["10.10.7.0/24","10.10.8.0/24","10.10.9.0/24"]
+  description = "db_subnets"
+}
+
 
